c4fc4e932f853528c877eccc5e1b0eddf30ae184
Vilde Gustafsson Bagstevold <vilde@Vilde-sin-MacBook-Pro.local>
2026-05-14 17:08:29 +0200

    Add 30% cut-off script

--- r_analysis/01_assign_comp.R
+++ r_analysis/01_assign_comp.R
@@ -29,5 +29,26 @@ hic <- HiCG %>%
     comp_liver = ifelse(E1_liver > 0, "A", "B")
   )
 
+# 30% thresholds
+q_low_gill   <- quantile(hic$E1_gill,  0.30, na.rm = TRUE)
+q_high_gill  <- quantile(hic$E1_gill,  0.70, na.rm = TRUE)
+
+q_low_liver  <- quantile(hic$E1_liver, 0.30, na.rm = TRUE)
+q_high_liver <- quantile(hic$E1_liver, 0.70, na.rm = TRUE)
+
+hic <- hic %>%
+  mutate(
+    comp30_gill = case_when(
+      E1_gill <= q_low_gill  ~ "B",
+      E1_gill >= q_high_gill ~ "A",
+      TRUE ~ "Ambiguous"
+    ),
+    comp30_liver = case_when(
+      E1_liver <= q_low_liver  ~ "B",
+      E1_liver >= q_high_liver ~ "A",
+      TRUE ~ "Ambiguous"
+    )
+  )
+
 # Save clean output
 write_tsv(hic, "data/derived/hic_compartments_250kb.tsv")
